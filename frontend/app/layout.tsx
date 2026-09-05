ee667bdbc80ca1ed1e3ea7d73322155c6b66c15f
Kevin valencia <kevintovarvalencia21@gmail.com>
2026-09-04 22:21:42 -0500

    feature: agrega e importa y renderiza Navbar en el layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
+import Navbar from "@/components/layout/Navbar";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Header />
         {children}
+        <Navbar />
       </body>
     </html>
   );
